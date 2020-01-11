9dd8a2daaeac7b26dcf2b8dfcad04dad873a82cc
Winifred Frierson <winifred.dykes@hotmail.com>
2020-01-11 06:52:21 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,37 @@
+class Artist
+  attr_accessor :name
+  
+  @@song_count = 0
+  
+  def initialize(name)
+    @name= name
+    @songs= []
+  end
+  
+  def add_song(song)
+    self.songs << song
+    song.artist = self
+    @@song_count += 1
+  end
+  
+  def add_song_by_name(name)
+    song = Song.new(name)
+    self.songs << song
+    song.artist = self
+    @@song_count +=1
+  end
+  
+  def songs
+    Song.all.select do |song|
+      song.artist == self
+    end  
+  end
+  
+  def self.song_count
+    Song.all.count
+  end
+end
+
+
+  
+  
